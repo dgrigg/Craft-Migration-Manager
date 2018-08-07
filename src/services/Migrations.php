b1e347fc97888a81a542d047c7fdd2f72f0810c6
Harald Atteneder <harald@urbantrout.io>
2018-08-07 09:27:53 +0200

    use Craft helpers instead of external tools

--- src/services/Migrations.php
+++ src/services/Migrations.php
@@ -6,9 +6,9 @@ use Craft;
 use craft\base\Component;
 use craft\helpers\App;
 use craft\helpers\FileHelper;
+use craft\helpers\StringHelper;
 use firstborn\migrationmanager\MigrationManager;
 use DateTime;
-use ForceUTF8\Encoding;
 
 class Migrations extends Component
 {
@@ -234,7 +234,7 @@ class Migrations extends Component
      */
     public function import($data)
     {
-        $data = iconv('UTF-8', 'UTF-8//IGNORE', Encoding::toUTF8($data));
+        $data = iconv('UTF-8', 'UTF-8//IGNORE', StringHelper::convertToUtf8($data));
         $data = json_decode($data, true);
         if (json_last_error() != JSON_ERROR_NONE){
             Craft::error('Migration Manager JSON error');
